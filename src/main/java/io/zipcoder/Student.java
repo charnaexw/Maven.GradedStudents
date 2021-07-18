0741459a640c50d43aca83eea837ed679f8c5849
charnaexw <charnaexw@gmail.com>
2021-07-18 15:22:49 -0400

    finished Students

--- src/main/java/io/zipcoder/Student.java
+++ src/main/java/io/zipcoder/Student.java
@@ -1,4 +1,52 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+
 public class Student {
-}
+    String firstName;
+    String lastName;
+    ArrayList<Double> examScores;
+
+    public Student(String firstName, String lastName, Double[] testScores){
+
+    }
+    public  void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getFirstName() { return firstName; }
+
+    public void setLastName(String lastName) { this.lastName = lastName; }
+
+    public String getLastName() { return lastName; }
+
+    public int getNumberOfExamsTaken(){
+        int examsTaken = examScores.size();
+
+        return examsTaken;
+    }
+
+    public String getExamScores(){
+        String output= "Exam Scores: \n";
+
+        for(int i=0; i<this.examScores.size(); i++){
+            output+= String.format("  Exam %d -> %d\n", i+1, (int)Math.rint(this.examScores.get(i)));
+        }
+
+        return output;
+
+    }
+    public void addExamScore(double examScore){
+        this.examScores.add(examScore);}
+
+    public void setExamScores(int examNumber, double newScore){this.examScores.set(examNumber-1, newScore);
+
+    }
+    public double getAverageExamScore(){
+        return examScores.size()/2;
+    }
+    public String toString(){
+        return String.format("Student Name: %s %s\n> Average Score: %d\n> ",this.firstName, this.lastName, (int)Math.rint(this.getAverageExamScore())) + this.getExamScores();
+    }
+
+    }
--- src/test/java/io/zipcoder/StudentTest.java
+++ src/test/java/io/zipcoder/StudentTest.java
@@ -1,5 +1,34 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
 public class StudentTest {
+
+    @Test
+    public void setFirstNameTest(){
+        //given
+        Student student = new Student;
+        String firstName= "Saitama";
+        String expected= firstName;
+        student.setFirstName(firstName);
+        //when
+        String actual= student.getFirstName();
+        //then
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void getExamScoresTest(){
+        // : Given
+        Double[] testScores = { 100.0, 95.0, 123.0, 96.0 };
+        Student student = new Student(null, null, testScores );
+        // When
+
+
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
 
 }
