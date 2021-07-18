3483b4ded82c0a36a76e2ce555ecf9c38bc2d6ba
charnaexw <charnaexw@gmail.com>
2021-07-18 15:42:47 -0400

    What am I doing?

--- src/main/java/io/zipcoder/Classroom.java
+++ src/main/java/io/zipcoder/Classroom.java
@@ -1,5 +1,24 @@
 package io.zipcoder;
 
 public class Classroom {
+private Student[] student;
 
+public Classroom(){
+    this.students=new Student[30];
 }
+public Classroom(int maxNumberOfStudents){
+    this.student= new Student[maxNumberOfStudents];
+}
+public Classroom(Student[] students){
+    this.student=students;
+}
+
+    public Student[] getStudent() {
+        return student;
+    }
+
+    public double getAverageExamScore(){
+    double
+    }
+}
+
